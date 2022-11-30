9e2df49eccb68a164b14c94bc179d8c71079e827
Megha P <megha.p@experionglobal.local>
2022-11-30 08:49:00 -0800

    add payment

--- routes/booking.js
+++ routes/booking.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 
 router.get('/', controller.bookingIndex);
-router.get('/create', controller.bookingCreate);
-router.post('/create', controller.bookingCreatePost);
+router.get('/create/:cab_no', controller.bookingCreate);
+router.post('/create/:cab_no', controller.bookingCreatePost);
 router.get('/update/:booking_id', controller.bookingUpdate);
 router.post('/update/:booking_id', controller.bookingUpdatePost);
 router.get('/delete/:booking_id', controller.bookingdelete);
-router.get('/booking', controller.booking);
-router.post('/booking', controller.booking);
+router.get('/payment/:cab_no', controller.payment);
+router.get('/paymentInvoice/:cab_no', controller.paymentInvoice);
 
 module.exports = router;
