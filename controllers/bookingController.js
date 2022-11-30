8e786665f7bf95ec04a6b122fa34f7aca4ff1dbd
Megha P <megha.p@experionglobal.local>
2022-11-30 08:46:32 -0800

    added payment

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,4 +1,6 @@
 const Booking = require('../models/booking');
+const Cab = require('../models/cab');
+
 
 module.exports.bookingIndex = ( req, res, next)=>{
     Booking.findAll().then(booking=>{
@@ -10,37 +12,31 @@ module.exports.bookingIndex = ( req, res, next)=>{
 module.exports.booking = (req, res, next)=>{
     res.render('booking');
 }
-module.exports.bookingpost = (req, res, next)=>{
-    Booking.create({
-        date_of_booking: req.body.dateofbooking, 
-        cab_from: req.body.cabfrom,
-        cab_to: req.body.cabto,
-        booking_time: req.body.bookingtime, 
-    })
-    .then(user =>{
-        res.redirect('/')
-    })
-}
-
 module.exports.bookingCreate = (req, res, next)=>{
-    res.render('booking-create');
+    res.render('booking');
 }
 
 module.exports.bookingCreatePost = (req, res, next)=>{
-    Booking.create({
-        // booking_id: req.body.bookingid, 
-        date_of_booking: req.body.dateofbooking, 
-        cab_from: req.body.cabfrom,
-        cab_to: req.body.cabto,
-        booking_time: req.body.bookingtime, 
-        cab_no: req.body.cabno, 
-        cost: req.body.cost, 
-        passenger_id: req.body.passengerid, 
-        driver_id: req.body.driverid
+    Cab.findByPk(req.params.cab_no).then((data)=>{
+        // console.log(data.driver_id);
+        // console.log('🚗🚗🚗🚗');
+        // console.log(req.identity.Passenger)
+        Booking.create({
+            // booking_id: req.body.bookingid, 
+            date_of_booking: req.body.dateofbooking, 
+            cab_from: req.body.cabfrom,
+            cab_to: req.body.cabto,
+            booking_time: req.body.bookingtime, 
+            cab_no: req.params.cab_no, 
+            cost: data.dataValues.cost,
+            passenger_id: req.identity.Passenger.passenger_id, 
+            driver_id: data.driver_id 
+        })
+        .then(user =>{
+            res.redirect('/booking/payment/'+req.params.cab_no)
+        })
     })
-    .then(user =>{
-        res.redirect('/')
-    })
+    
 }
 
 module.exports.bookingUpdate = (req, res, next)=>{
@@ -81,4 +77,17 @@ module.exports.bookingdelete = async(req, res, next)=>{
         });
         res.redirect('/');
     }
+}
+
+
+module.exports.payment = async(req, res, next)=>{
+    var payment = await Booking.findOne({where: {cab_no: req.params.cab_no}})
+    res.render('payment',
+    {
+        data:payment
+    })
+}
+
+module.exports.paymentInvoice = async (req, res, next)=>{
+    res.render('invoice')
 }
